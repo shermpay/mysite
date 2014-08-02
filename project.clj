c606a815d74e216fa1005fdde219b08c4a0b8a6d
shermpay <shermanpay1991@gmail.com>
2014-08-02 13:33:54 -0700

    Updated version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mysite "0.1.0-SNAPSHOT"
+(defproject mysite "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
