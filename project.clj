3c2ac2adff58e19d70e877cb5c277df03e2ca74c
Sherman Pay <shermanpay1991@gmail.com>
2014-09-12 14:40:25 -0700

    Changes to project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mysite "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Sherman Pay's personal website."
+  :url "http://shermanpay.com/"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
