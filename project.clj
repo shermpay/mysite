3a5b880e085c1cb21dea015bceeb1f00359dfb00
shermpay <shermanpay1991@gmail.com>
2015-01-26 09:05:52 -0800

    Updated deps

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [hiccup "1.0.5"]
-                 [ring-server "0.3.1"]
+                 [ring-server "0.4.0"]
                  [org.clojure/java.jdbc "0.3.6"]
                  [mysql/mysql-connector-java "5.1.34"]
-                 [markdown-clj "0.9.61"]]
+                 [markdown-clj "0.9.62"]]
   :resource-paths ["resources"]
   :plugins [[lein-ring "0.8.13"]
             [lein-cljsbuild "1.0.3"]
@@ -26,5 +26,5 @@
     {:open-browser? false, :stacktraces? false, :auto-reload? false}}
    :dev
    {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.3.2"]
-                   [org.clojure/clojurescript "0.0-2629"]
+                   [org.clojure/clojurescript "0.0-2727"]
                    [figwheel "0.2.0-SNAPSHOT"]]}})
